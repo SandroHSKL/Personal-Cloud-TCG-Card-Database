d5b3ef944b189bbba9f83f1ac4b3010202e5d223
Kloos, Sandro <sakl1001@stud.hs-kl.de>
2025-07-09 11:41:10 +0200

    spring initalizer

---
+++ PCTCD/src/main/java/com/irgendwas/PCTCD/PctcdApplication.java
@@ -0,0 +1,13 @@
+package com.irgendwas.PCTCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PctcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PctcdApplication.class, args);
+	}
+
+}
---
+++ PCTCD/src/test/java/com/irgendwas/PCTCD/PctcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irgendwas.PCTCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PctcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
